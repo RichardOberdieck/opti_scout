d4889f57c214847347f194d0f63ef07a0839b9d0
Torben Skov <astskov@gmail.com>
2026-02-22 22:35:42 +0100

    real testdata

--- opti_scout/build_model.py
+++ opti_scout/build_model.py
@@ -1,7 +1,7 @@
 from opti_scout.classes import AssigningActivititesProblem, Solution
 from pydantic import BaseModel
 from mip import BINARY, xsum, Model, maximize, Var
-import os
+import os, time, datetime
 
 class ModelBuilder(BaseModel, arbitrary_types_allowed=True):
    assigning_activities_problem: AssigningActivititesProblem
@@ -12,21 +12,95 @@ class ModelBuilder(BaseModel, arbitrary_types_allowed=True):
        return cls(assigning_activities_problem=assigning_activities_problem, model=Model())
 
 
-   def solve(self) -> Solution:
+   def solve(self, maxseconds : int, filename : str) -> Solution:
+      
       x = self.generate_variables()
-
+      
+      print ("filename: " + filename)
+      print ("#Activities: " + str(len(self.assigning_activities_problem.activities)))
+      print ("#Groups: " + str(len(self.assigning_activities_problem.groups)))
+      print ("#selections: " + str(len(self.assigning_activities_problem.selections)))
+      time.sleep(5)
+      starttime = datetime.datetime.now()
+      print ("add_maxscout_constraint (1/8)")
       self.add_maxscout_constraint(x)
+      addmax_scout_time = datetime.datetime.now()
+      print ("add_max_1_session_constraint (2/8)")
       self.add_max_1_session_constraint(x)
+      addmax_1_session_time = datetime.datetime.now()
+      print ("add_max_1_of_overlapping_sessions_constraint (3/8)")
       self.add_max_1_of_overlapping_sessions_constraint(x)
+      addmax_1_overlapping_time = datetime.datetime.now()
+      print ("add_unavailable_time_constraint (4/8)")
       self.add_unavailable_time_constraint(x)
+      add_unavailable_time = datetime.datetime.now()
+      print ("add_onlyone_activitylocation_eachday_constraint (5/8)")
       self.add_onlyone_activitylocation_eachday_constraint(x)
+      add_location_eachday_time = datetime.datetime.now()
+      print ("add_age_constraint (6/8)")
       self.add_age_constraint(x)
+      add_age_time = datetime.datetime.now()
       #number is the maxmimum of popular activities
+      print ("add_max_nb_of_most_popular_activities_constraint (7/8)")
       self.add_max_nb_of_most_popular_activities_constraint(x, 2)
-
+      add_max_popular_time = datetime.datetime.now()
+      print ("add_objective (8/8)")
       self.add_objective(x)
+      add_objective_time = datetime.datetime.now()
+      modelready_time = datetime.datetime.now()
 
-      status = self.model.optimize(max_seconds=300)
+      print ("filename: " + filename)
+      print ("#Activities: " + str(len(self.assigning_activities_problem.activities)))
+      print ("#Groups: " + str(len(self.assigning_activities_problem.groups)))
+      print ("#selections: " + str(len(self.assigning_activities_problem.selections)))
+
+      print ("starttime:" + starttime.strftime("%H:%M:%S"))
+      print("modelready_time:" + modelready_time.strftime("%H:%M:%S"))
+      print ("endtime starttime diff:" + str(int((modelready_time-starttime).total_seconds() // 60))+ " minutes")
+
+      print ("addmax_scout_time: " + addmax_scout_time.strftime("%H:%M:%S") + "(" + str(int((addmax_scout_time-starttime).total_seconds() // 60)) + " minutes)")
+      print ("addmax_1_session_time: " + addmax_1_session_time.strftime("%H:%M:%S") + "(" + str(int((addmax_1_session_time-addmax_scout_time).total_seconds() // 60)) + " minutes)")
+      print ("addmax_1_overlapping_time: " + addmax_1_overlapping_time.strftime("%H:%M:%S") + "(" + str(int((addmax_1_overlapping_time-addmax_scout_time).total_seconds() // 60)) + " minutes)")
+      print ("add_unavailable_time: " + add_unavailable_time.strftime("%H:%M:%S") + "(" + str(int((add_unavailable_time-addmax_1_overlapping_time).total_seconds() // 60)) + " minutes)")
+      print ("add_1_location_eachday_time: " + add_location_eachday_time.strftime("%H:%M:%S") + "(" + str(int((add_location_eachday_time-add_unavailable_time).total_seconds() // 60)) + " minutes)")
+      print ("add_age_time: " + add_age_time.strftime("%H:%M:%S") + "(" + str(int((add_age_time-add_location_eachday_time).total_seconds() // 60)) + " minutes)")
+      print ("add_max_popular_time: " + add_max_popular_time.strftime("%H:%M:%S") + "(" + str(int((add_max_popular_time-add_age_time).total_seconds() // 60)) + " minutes)")
+      print ("add_objective_time: " + add_objective_time.strftime("%H:%M:%S") + "(" + str(int((add_objective_time-add_max_popular_time).total_seconds() // 60)) + " minutes)")
+      print ("modelready_time: " + modelready_time.strftime("%H:%M:%S") + "(" + str(int((modelready_time-add_objective_time).total_seconds() // 60)) + " minutes)")
+      print ("modelready_time total: " + modelready_time.strftime("%H:%M:%S") + "(" + str(int((modelready_time-starttime).total_seconds() // 60)) + " minutes)")
+      
+      print("preproces")
+      print(self.model.preprocess)
+      #print("set preproces to 0")
+      #self.model.preprocess=0
+
+      self.model.write(filename+".lp")
+      self.model.write(filename+".mps")
+      
+      status = self.model.optimize(max_seconds=maxseconds)
+      endtime = datetime.datetime.now()
+
+
+      print ("#Activities: " + str(len(self.assigning_activities_problem.activities)))
+      print ("#Groups: " + str(len(self.assigning_activities_problem.groups)))
+      print ("#selections: " + str(len(self.assigning_activities_problem.selections)))
+
+      print ("starttime:" + starttime.strftime("%H:%M:%S"))
+      print("endtime:" + endtime.strftime("%H:%M:%S"))
+      print ("endtime starttime diff:" + str(int((endtime-starttime).total_seconds() // 60))+ " minutes")
+
+      print ("addmax_scout_time: " + addmax_scout_time.strftime("%H:%M:%S") + "(" + str(int((addmax_scout_time-starttime).total_seconds() // 60)) + " minutes)")
+      print ("addmax_1_session_time: " + addmax_1_session_time.strftime("%H:%M:%S") + "(" + str(int((addmax_1_session_time-addmax_scout_time).total_seconds() // 60)) + " minutes)")
+      print ("addmax_1_overlapping_time: " + addmax_1_overlapping_time.strftime("%H:%M:%S") + "(" + str(int((addmax_1_overlapping_time-addmax_scout_time).total_seconds() // 60)) + " minutes)")
+      print ("add_unavailable_time: " + add_unavailable_time.strftime("%H:%M:%S") + "(" + str(int((add_unavailable_time-addmax_1_overlapping_time).total_seconds() // 60)) + " minutes)")
+      print ("add_1_location_eachday_time: " + add_location_eachday_time.strftime("%H:%M:%S") + "(" + str(int((add_location_eachday_time-add_unavailable_time).total_seconds() // 60)) + " minutes)")
+      print ("add_age_time: " + add_age_time.strftime("%H:%M:%S") + "(" + str(int((add_age_time-add_location_eachday_time).total_seconds() // 60)) + " minutes)")
+      print ("add_max_popular_time: " + add_max_popular_time.strftime("%H:%M:%S") + "(" + str(int((add_max_popular_time-add_age_time).total_seconds() // 60)) + " minutes)")
+      print ("add_objective_time: " + add_objective_time.strftime("%H:%M:%S") + "(" + str(int((add_objective_time-add_max_popular_time).total_seconds() // 60)) + " minutes)")
+      print ("modelready_time: " + modelready_time.strftime("%H:%M:%S") + "(" + str(int((modelready_time-add_objective_time).total_seconds() // 60)) + " minutes)")
+      print ("modelsolved_time: " + endtime.strftime("%H:%M:%S") + "(" + str(int((endtime - modelready_time).total_seconds() // 60)) + " minutes)")
+      
+
       return Solution.build(x, status)
 
 
@@ -38,8 +112,12 @@ class ModelBuilder(BaseModel, arbitrary_types_allowed=True):
    #any session of an activity cannot have more than the max number of participants for that session (capacity may  vary for each session of an activity)
    def add_maxscout_constraint(self, x: dict[tuple, Var]) -> None:
       print ("add_maxscout_constraint")
+      activitycounter=0
       for a in self.assigning_activities_problem.activities:
-         for activity_session in a.timeslots:
+         activitycounter=activitycounter+1
+         if activitycounter % 10 == 0:
+            print (a.id)
+            for activity_session in a.timeslots:
                #print("one activity session")
                #print (a.id, activity_session.id)
                selections = {
@@ -53,7 +131,11 @@ class ModelBuilder(BaseModel, arbitrary_types_allowed=True):
    # one group gets at most one session from any activity
    def add_max_1_session_constraint(self, x: dict[tuple, Var]) -> None:
       print("add_max_1_session_constraint")
+      grpcounter=0
       for g in self.assigning_activities_problem.groups:
+         grpcounter=grpcounter+1
+         if grpcounter % 25 == 0:
+            print (g.id)
          for a in self.assigning_activities_problem.activities:
             selections = [
                s for s in self.assigning_activities_problem.selections if s.group == g and s.activity == a
@@ -68,7 +150,11 @@ class ModelBuilder(BaseModel, arbitrary_types_allowed=True):
    # any group can at most have 1 session of overlapping sessions across all activities that they have prioritized
    def add_max_1_of_overlapping_sessions_constraint(self, x: dict[tuple, Var]) -> None:
       print("add_max_1_of_overlapping_sessions_constraint")
+      sessioncounter=0
       for s in self.assigning_activities_problem.selections:
+         sessioncounter=sessioncounter+1
+         if sessioncounter % 1000 == 0:
+            print (sessioncounter)
          for s1 in self.assigning_activities_problem.get_overlapping_selections(s):
             self.model += (
                x[s] + x[s1] <= 1,
@@ -80,13 +166,17 @@ class ModelBuilder(BaseModel, arbitrary_types_allowed=True):
    # This could be avoided if we only generated variables representing sessions in available timeslots for groups
    def add_unavailable_time_constraint(self, x: dict[tuple, Var]) -> list[tuple]:
       print("add_unavailable_time_constraint")      
+      sessioncounter=0
       for s in self.assigning_activities_problem.selections:
+         sessioncounter=sessioncounter+1
+         if sessioncounter % 1000 == 0:
+               print (sessioncounter)
          if s.group.in_available_timeslots(s.time_slot):
                continue
 
          self.model += (
                x[s] <= 0,
-               f"groupDoesNotHaveAvailabletime_{s.group.id}_{s.activity.id}_start{s.time_slot.startname()}",
+               f"groupDoesNotHaveAvailabletime_{s.group.id}_{s.activity.id}_{s.time_slot.id}",
          )
 
 
@@ -95,8 +185,11 @@ class ModelBuilder(BaseModel, arbitrary_types_allowed=True):
    # A lot of 2 seleciton constraints
    def add_onlyone_activitylocation_eachday_constraint(self, x: dict[tuple, Var]) -> None:
       print("add_onlyone_activitylocation_eachday_constraint")
+      sessioncounter=0
       for s in self.assigning_activities_problem.selections:
-
+         sessioncounter=sessioncounter+1
+         if sessioncounter % 1000 == 0:
+               print (sessioncounter)
          selections = self.assigning_activities_problem.get_all_selections_on_other_locations_for_different_activities_same_day(s)
          for s1 in selections:
                self.model += x[s] + x[s1] <= 1, f"{s}_and_{s1}_excluded_because_different_location_same_day_{s.activity}"
@@ -107,21 +200,34 @@ class ModelBuilder(BaseModel, arbitrary_types_allowed=True):
     #this should not happen for any, as data is supposed to be cleaned, keep it as a check
    def add_age_constraint(self, x: dict[tuple, Var]) -> list[tuple]:
       print("add_age_constraint")
+      sessioncounter=0
+      printmsg=1
       for s in self.assigning_activities_problem.selections:
+         sessioncounter=sessioncounter+1
+         if sessioncounter % 1000 == 0:
+               print (sessioncounter)
          if s.group.age_span.low >= s.activity.age_span.low and s.group.age_span.high <= s.activity.age_span.high :
                continue
 
-         print(f"NOTE found selection where age group not with allowed ages {s.group.id} {s.activity.id}, this should not be the case, adding constraint to prevent assignment")
-         self.model += (
-               x[s] <= 0,
-               f"group_{s.group.id}_does_not_have_age_required_for_{s.activity.id}_{s.time_slot.id}_{s.time_slot.startname()}",
-         )
+         if printmsg == 1:
+            print(f"NOTE found selection where age group not with allowed ages {s.group.id} {s.activity.id}, this should not be the case, adding constraint to prevent assignment")
+            print(f"Remember this is disabled")
+            printmsg=0
+
+         #self.model += (
+         #      x[s] <= 0,
+         #      f"group_{s.group.id}_does_not_have_age_required_for_{s.activity.id}_{s.time_slot.id}_{s.time_slot.startname()}",
+         #)
 
     # find most popular activities and constrain to max_activities
     # another constraint will make sure at the most one session for one activity
    def add_max_nb_of_most_popular_activities_constraint(self, x: dict[tuple, Var],max_activities: int) -> list[tuple]:
       print("add_max_nb_of_most_popular_activities_constraint")
+      grpcounter=0
       for g in self.assigning_activities_problem.groups:
+         grpcounter=grpcounter+1
+         if grpcounter % 25 == 0:
+            print (g.id)
          selections = [
                s for s in self.assigning_activities_problem.selections if s.group == g and s.activity in self.assigning_activities_problem.popularactivities
             ]
